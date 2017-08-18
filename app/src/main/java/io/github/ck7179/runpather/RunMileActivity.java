5ba7530318ca659a945c2d5b9013418a07a85d52
whitehot7179 <mryang7179@gmail.com>
2017-08-19 00:24:36 +0800

    1.建立一個新activity(RunMileActivity)，設定與RunEnvirActivity相似的頁面 2.實作並調整activity之間的跳轉，使用了intent的flg功能

--- app/src/main/java/io/github/ck7179/runpather/RunMileActivity.java
+++ app/src/main/java/io/github/ck7179/runpather/RunMileActivity.java
@@ -1,42 +1,29 @@
 package io.github.ck7179.runpather;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 
-public class RunEnvirActivity extends AppCompatActivity {
+public class RunMileActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Button button_next;
-    private FloatingActionButton fab;
 
     private void findViews() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         button_next = (Button) findViewById(R.id.button_next);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run_envir);
+        setContentView(R.layout.activity_run_mile);
         findViews();
         set_toolbar();;
-        set_fabClick();
         setNextButton();
     }
 
@@ -46,22 +33,10 @@ public class RunEnvirActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        //設定返回鍵按下的處裡
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 lastpage();
-            }
-        });
-    }
-
-    //設定按下fab浮動按鈕
-    public void set_fabClick(){
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //.setAction("Action", null).show();
             }
         });
     }
@@ -78,8 +53,7 @@ public class RunEnvirActivity extends AppCompatActivity {
 
     //上一頁intent
     public void lastpage(){
-        Intent intent = new Intent(this,MainActivity.class);
-        //finish在此activity之上層(包含此層)的activity
+        Intent intent = new Intent(this,RunEnvirActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         //不另create新的activity，只把目標activity移到上層
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
